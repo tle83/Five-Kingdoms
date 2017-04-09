f22018cce75409aed3c51f81bf3dfb2f0d7206e7
tle83 <tnle833@gmail.com>
2017-04-08 23:40:00 -0500

    Add Health UI and Stamina

---
+++ Five Kingdoms/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int initialHP = 100;
+	public int currentHP;
+	public Slider hpSlider;
+	public Image DMG;
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+	PlayerController playerMovement;
+	bool isDead;
+	bool damaged;
+
+	// Use this for initialization
+	void Start () {
+		playerMovement = GetComponent<PlayerController> ();
+
+		currentHP = initialHP;
+	}
+	
+	// Update is called once per frame
+	void Update () 	{
+		if (damaged) {
+			DMG.color = flashColor;
+		} else {
+			DMG.color = Color.Lerp (DMG.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
+
+	public void TakeDamage(int amount){
+		damaged = true;
+		currentHP -= amount;
+		hpSlider.value = currentHP;
+		if (currentHP <= 0 && !isDead) {
+			Death ();
+		}
+	}
+
+	void Death(){
+		isDead = true;
+		playerMovement.enabled = false;
+	}
+}
